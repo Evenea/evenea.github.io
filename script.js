0b02a126b77369d3175bb6a7d03a502fb4c2aa10
Evenea <Linnea@Linneas-MacBook-Air.local>
2017-03-19 09:05:56 +0100

    För uppladdning

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("js-menu​").addEventListener("click", function() {
+	document.getElementById("js-wrapper​").classList.toggle("show-menu");
+	document.getElementById("js-nav​").classList.toggle("visible");
+});
